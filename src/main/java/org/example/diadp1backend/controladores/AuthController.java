f48f233bb168dbef263d0f9e96d6542cc0b1addf
Vilches <victormvg21@gmail.com>
2025-02-06 11:56:39 +0100

    Fallos, por revertir

--- src/main/java/org/example/diadp1backend/controladores/AuthController.java
+++ src/main/java/org/example/diadp1backend/controladores/AuthController.java
@@ -1,23 +1,24 @@
-package org.example.diadp1backend.controladores;
-
-import lombok.AllArgsConstructor;
-import org.example.diadp1backend.DTOs.RegistroDTO;
-import org.example.diadp1backend.modelos.Usuario;
-import org.example.diadp1backend.servicios.UsuarioService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/auth")
-@AllArgsConstructor
-public class AuthController {
-
-  private UsuarioService usuarioService;
-
-  @PostMapping("/registro")
-  public Usuario registro(RegistroDTO registroDTO){
-
-    return usuarioService.registrarUsuario(registroDTO);
-  }
-}
+// package org.example.diadp1backend.controladores;
+//
+// import lombok.AllArgsConstructor;
+// import org.example.diadp1backend.DTOs.RegistroDTO;
+// import org.example.diadp1backend.modelos.Usuario;
+// import org.example.diadp1backend.servicios.UsuarioService;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// @RequestMapping("/auth")
+// @AllArgsConstructor
+// public class AuthController {
+//
+//   private UsuarioService usuarioService;
+//
+//   @PostMapping("/registro")
+//   public Usuario registro(RegistroDTO registroDTO){
+//
+//     return usuarioService.registrarUsuario(registroDTO);
+//   }
+// }
+//
